9436342c7e6077cbd75ca3960f4702d32cb42614
iAveed <aveed.clips@gmail.com>
2023-07-31 11:48:38 +0400

    Initial Commit

---
+++ Todo app/Todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appApp.swift
+//  Todo app
+//
+//  Created by Aveed on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
